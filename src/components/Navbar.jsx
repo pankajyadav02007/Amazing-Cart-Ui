432014efdf3af3cd00badded1c13a410fd4e180d
pankaj <pankajyadav221306@gmail.com>
2025-04-26 16:52:33 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Menu, Search, ShoppingBag, ShoppingBagIcon, ShoppingCart, User, User2 } from 'lucide-react'
+import { Heart, LogIn, LogInIcon, LucideLogIn, Menu, Search, SearchCheck, ShoppingBag, ShoppingBagIcon, ShoppingCart, User, User2 } from 'lucide-react'
 import React from 'react'
 import { NavLink } from 'react-router'
 
 const Navbar = () => {
   return (
 <>
-<section className='h-15 bg-amber-100 md:w-[100%]  flex items-center gap-25 md:justify-between pr-0 pl-5  md:pr-10 md:pl-10 z-10 sticky top-0 left-0 right-0'>
+<section className='h-15 bg-amber-100 md:w-[100%]  flex items-center gap-25 md:justify-between  p-5  md:pr-10 md:pl-10 z-10 sticky top-0 left-0 right-0'>
 <div className='flex justify-center items-center  gap-2'>
          <img className='h-[100%] w-20  object-cover p-2 rounded-2xl border-white bg-white' src='https:static.vecteezy.com/system/resources/previews/026/426/593/non_2x/carpet-design-texture-persian-and-tebriz-rugs-design-print-fabric-design-vector.jpg'/>
        <h1 className='text-xl  md:text-3xl '>Rugs</h1>
@@ -19,7 +19,7 @@ const Navbar = () => {
         <User2 className='h-[32px] w-[32px] p-1 border rounded-2xl cursor-pointer'/>
         </div>
 </section>
-     <section className='h-12 md:h-18 bg-blue-300   flex  items-center md:justify-center pl-5 sticky top-0 left-0 right-0'>
+     <section className='h-12 md:h-18 bg-blue-300   flex  items-center justify-between md:justify-center p-5 sticky top-0 left-0 right-0'>
        
        <div className='md:flex items-center justify-center gap-15 text-[18px] hidden font-semibold'>
        <NavLink className={({isActive})=>`text-red-400  ${isActive}` }>
@@ -39,8 +39,9 @@ const Navbar = () => {
        </NavLink>
        </div>
        <Menu className='md-screen md:hidden'/>
-       <div>
-
+       <div className='md:hidden flex gap-3'>
+        <Search/>
+        <Heart/>
        </div>
      </section>
     </>
