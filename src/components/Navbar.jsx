72a9920631048b21768104d09792aec06457a6fd
pankaj <pankajyadav221306@gmail.com>
2025-04-26 06:13:36 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router'
 const Navbar = () => {
   return (
 <>
-<section className='h-15 bg-amber-100 md:w-[100%]  flex items-center gap-25 md:justify-between pr-0 pl-0  md:pr-10 md:pl-10 z-10 sticky top-0 left-0 right-0'>
+<section className='h-15 bg-amber-100 md:w-[100%]  flex items-center gap-25 md:justify-between pr-0 pl-5  md:pr-10 md:pl-10 z-10 sticky top-0 left-0 right-0'>
 <div className='flex justify-center items-center  gap-2'>
          <img className='h-[100%] w-20  object-cover p-2 rounded-2xl border-white bg-white' src='https:static.vecteezy.com/system/resources/previews/026/426/593/non_2x/carpet-design-texture-persian-and-tebriz-rugs-design-print-fabric-design-vector.jpg'/>
        <h1 className='text-xl  md:text-3xl '>Rugs</h1>
